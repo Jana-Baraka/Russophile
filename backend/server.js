2693ebe347ab3b5ca37e2ac3b30071152bc382fe
JanaBaraka <95126147+Jana-Baraka@users.noreply.github.com>
2024-12-13 14:08:45 +0200

    cors error from port 3000 to 3001

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const authRoutes = require('./routes/auth');
 const lessonRoutes = require('./routes/lessons');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000', credentials: true })); 
+app.use(cors({ origin: 'http://localhost:3001', credentials: true })); 
 app.use(express.json());
 app.use(cookieParser());
 
