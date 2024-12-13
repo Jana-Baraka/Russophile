2e2df5809e07b0fee93a0fa9dfed6f980157b783
JanaBaraka <95126147+Jana-Baraka@users.noreply.github.com>
2024-12-13 04:00:03 +0200

    update, added LINK to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import LessonsPage from './pages/LessonsPage';
