1a2b94941ffb53970f5215486fd7704ab7cd560e
JanaBaraka <95126147+Jana-Baraka@users.noreply.github.com>
2024-12-14 17:04:49 +0200

    updated auth js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, logout } = require('../controllers/authController');
+const { getCurrentUser } = require('../controllers/authController');
+const { authenticate } = require('../middleware/authMiddleware');
+
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
+router.get('/me', authenticate, getCurrentUser);
 
 module.exports = router;
