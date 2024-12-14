f21dd603bb357e0459a2c3e6c9dfb2ff4a4476bd
JanaBaraka <95126147+Jana-Baraka@users.noreply.github.com>
2024-12-14 17:31:42 +0200

    Update progress.js

--- backend/routes/progress.js
+++ backend/routes/progress.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authenticate } = require('../middlewares/authMiddleware');
+const { authenticate } = require('../middleware/authMiddleware');
 const { startTrack, updateProgress, deleteTrack, getProgress } = require('../controllers/progressController');
 
 console.log("Progress routes loaded");
